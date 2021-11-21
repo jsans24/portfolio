189ba42e735610d763745cf68a4830abe1e14d47
jsans24@gmail.com <jsans24@gmail.com>
2021-11-21 11:14:07 -0800

    reworked layout of app.js
    Signed-off-by: jsans24@gmail.com <jsans24@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import './App.css';
@@ -7,10 +7,8 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <Routes>
-        <Route exact path='/' element={ <Home/> }/>
-        <Route path='/projects' element={ <Projects/> }/>
-      </Routes>
+      <Home />
+      <Projects />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
     return (
-        <h1>
+        <h1 className="home">
             Josua Sanford
         </h1>
     );
