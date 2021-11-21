bad709f641936a60cba8328306930e327b783abd
jsans24@gmail.com <jsans24@gmail.com>
2021-11-21 10:36:20 -0800

    put placeholder in projects component
    Signed-off-by: jsans24@gmail.com <jsans24@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Projects from './components/Projects';
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="container">
       <Routes>
         <Route exact path='/' element={ <Home/> }/>
-        {/* <Route path='/projects' element={ <Projects/> }/> */}
+        <Route path='/projects' element={ <Projects/> }/>
       </Routes>
     </div>
   );
---
+++ src/components/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+        <h1>
+            something else
+        </h1>
+    );
+};
+export default Projects;
