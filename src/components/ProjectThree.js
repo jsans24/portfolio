5a15e8ccf3d7646fce7b9aebfd5402c32a0dd848
jsans24@gmail.com <jsans24@gmail.com>
2021-11-21 15:56:41 -0800

    finished project one component functionality
    Signed-off-by: jsans24@gmail.com <jsans24@gmail.com>

--- src/components/ProjectThree.js
+++ src/components/ProjectThree.js
@@ -1,6 +1,5 @@
 import React from "react";
 import awepng from "../images/anywhereelselandingpage.png";
-import awegif from "../images/anywhereelse.gif";
 
 
 const ProjectOne = () => {
@@ -10,9 +9,7 @@ const ProjectOne = () => {
             <p>
                 A travel community site mockup built by a team of four devs using the django platform.
             </p>
-            <img src={awepng} alt="image of Anywhere. Else. landing page"
-                onMouseOver={e => (e.currentTarget.src = awegif)}
-                onMouseOut={e => (e.currentTarget.src = awepng)} width="25%"></img>
+            <img src={awepng} alt="image of Anywhere. Else. landing page" width="25%"></img>
         </div>
     );
 };
