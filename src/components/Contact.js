d71f30d066be9eda5459be9a9cd93f880d2ac399
jsans24@gmail.com <jsans24@gmail.com>
2021-11-21 23:08:51 -0800

    changed css styling
    Signed-off-by: jsans24@gmail.com <jsans24@gmail.com>

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,22 +3,22 @@ import React from "react";
 const Contact = () => {
     return (
         <section id="contact">
-            <h2>Reach Out</h2>\
-            <form action="mailto:jsans24@gmail.com" method="get" enctype="text/plain">
+            <h2>Reach Out</h2>
+            <form className="email" action="mailto:jsans24@gmail.com" method="get" enctype="text/plain">
                 <div>
                     <label for="name">Name:
-                        <input type="text" name="name" id="name" />
+                        <input type="text" name="name" id="name" size="50" />
                     </label>
                 </div>
                 <div>
                     <label for="email">Email:
-                        <input type="text" name="email" id="email" />
+                        <input type="text" name="email" id="email" size="50"/>
                     </label>
                 </div>
                 <div>
                     <label>Comments:</label>
                     <br />
-                    <textarea name="comments" rows="12" cols="35"></textarea>
+                    <textarea name="comments"></textarea>
                 </div>
                 <div>
                     <input type="submit" name="submit" value="Send" />
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,11 @@ import React from "react";
 const Home = () => {
     return (
         <section id="home">
-            <h1 className="home">
-                Josua Sanford
-            </h1>
+            <h1 className="home">Josua Sanford</h1>
+            <h3>Hi, I'm a full stack web developer from southern california</h3>
+
+            <h4>Languages I speak:</h4>
+            <p>Javascript, HTML, CSS, Sass, Python</p>
         </section>
     );
 };
